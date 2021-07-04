60577f0196fb86a5251cd21c18ff906c74360cfa
Fábio Fernandes <81197500+ffernandes2021@users.noreply.github.com>
2021-07-04 00:04:36 -0300

    Update Exercicio7.java

--- tecnicas-de-desenvolvimento-de-algoritmos-t1-t2/assets/exercises/java/Exercicio7.java
+++ tecnicas-de-desenvolvimento-de-algoritmos-t1-t2/assets/exercises/java/Exercicio7.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class Exercicio7 {
 
     public static void main(String[] args) {
+        
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
         System.out.print("Digite a temperatura em Fahrenheit: ");
@@ -11,6 +12,7 @@ public class Exercicio7 {
         double tempCelsius = (5 * (tempFahrenheit - 32) / 9);
         System.out.printf(Locale.US, "%.2fº F %n", tempFahrenheit);
         System.out.printf(Locale.US, "%.2fº C %n", tempCelsius);
+    
     }
 
 }
